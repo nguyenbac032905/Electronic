444c09f11a1ee0db3ce5527a7f42c80b05daea49
nguyen van bac <nguyenbac29305@gmail.com>
2026-06-07 21:28:04 +0700

    single_product_page_improved

--- app/product/[productSlug]/page.tsx
+++ app/product/[productSlug]/page.tsx
@@ -4,7 +4,7 @@ import { FaHeart, FaSquareFacebook, FaSquarePinterest, FaSquareXTwitter } from "
 
 const SingleProductPage = () => {
     return (
-        <div className="mx-auto flex max-w-screen-2xl justify-center gap-x-16 pt-10">
+        <div className="mx-auto flex max-w-screen-2xl justify-center gap-x-16 pt-10 max-lg:flex-col items-center gap-y-5 px-5">
             <div>
                 <Image src={"/laptop 1.webp"} width={500} height={500} alt="main image" />
                 <div className="flex justify-around mt-5">
--- components/SimpleSlider.tsx
+++ components/SimpleSlider.tsx
@@ -11,12 +11,32 @@ const SimpleSlider = () => {
         slidesToScroll:1
     };
     return(
-        <div className="max-w-screen-2xl mx-auto px-16 max-md:px-7 slider-container">
+        <div className="slider-container max-w-screen-2xl mx-auto px-16 max-md:px-7 w-full">
             <Slider {...setting}>
-                <div className="h-[500px] max-lg:h-[400px] max-md:h-[250px] max-[500px]:h-[200px] max-[400px]:h-[150px]">
-                    <img src="/slider image 1.webp" alt="slider 1" className="h-full"/>
+                <div className="h-[500px] max-lg:h-[400px] max-md:h-[250px] max-[500px]:h-[200px] max-[400px]:h-[150px] relative">
+                    <div className="absolute left-[50%] translate-x-[-50%] translate-y-[30%] h-full text-center max-lg:translate-y-[25%] max-sm:hidden">
+                        <p className="text-xl font-light underline underline-offset-4 mb-5 max-[1162px]:text-lg max-lg:mb-2 max-md:text-base max-md:mb-1">Headphones</p>
+                        <h2 className="text-5xl font-light mb-5 max-[1162px]:text-4xl max-lg:mb-2 max-lg:text-3xl max-md:text-2xl max-md:mb-1">Just to rock your mood</h2>
+                        <p className="mb-5 text-lg font-medium max-[1162px]:text-base max-lg:mb-2 max-lg:text-sm max-md:mb-1">Fine-tune your listening experience by using the convenient volume slider to adjust sound intensity to your preference.</p>
+                        <button className="btn w-52 h-14 bg-custom-yellow border border-black uppercase text-xl hover:bg-black hover:text-custom-yellow max-[1162px]:w-44 max-[1162px]:h-10 max-[1162px]:text-lg max-lg:text-base max-lg:w-36 max-md:text-sm max-md:w-32">Shop now</button>
+                    </div>
+                    <img src="/slider image 1.webp" alt="slider 1" className="w-full h-full object-cover"/>
                 </div>
-                <div className="h-[500px] max-lg:h-[400px] max-md:h-[250px] max-[500px]:h-[200px] max-[400px]:h-[150px]">
+                <div className="h-[500px] max-lg:h-[400px] max-md:h-[250px] max-[500px]:h-[200px] max-[400px]:h-[150px] relative">
+                    <div className="absolute left-[50%] translate-x-[-50%] translate-y-[30%] h-full text-center max-lg:translate-y-[25%] max-md:translate-y-[20%] max-sm:hidden">
+                        <p className="text-xl font-light underline underline-offset-4 mb-5 max-[1162px]:text-lg max-lg:mb-2 max-md:text-base max-md:mb-1">
+                            Earbud
+                        </p>
+                        <h2 className="text-5xl font-light mb-5 max-[1162px]:text-4xl max-lg:mb-2 max-lg:text-3xl max-md:text-2xl max-md:mb-1">
+                            Go wireless or go home
+                        </h2>
+                        <p className="mb-5 text-black text-lg font-medium max-[1162px]:text-base max-lg:mb-2 max-lg:text-sm max-md:mb-1">
+                            Seamlessly control your sound world by sliding the volume to your perfect level.
+                        </p>
+                        <button className="bg-custom-yellow btn w-52 h-14 text-black uppercase border border-black text-xl hover:bg-black hover:text-custom-yellow max-[1162px]:w-44 max-[1162px]:h-10 max-[1162px]:text-lg max-lg:text-base max-lg:w-36 max-md:text-sm max-md:w-32">
+                            Shop now
+                        </button>
+                    </div>
                     <img src="/slider image 2.webp" alt="slider 2" className="h-full"/>
                 </div>
             </Slider>
