a3a411d59f6b94a57229eee496baf2d83f918ed2
nguyen van bac <nguyenbac29305@gmail.com>
2026-06-10 16:06:30 +0700

    ket noi voi prisma

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
+import {CustomButton} from "@/components";
 import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 const ProductItem = () => {
     return (
         <div className="flex flex-col items-center gap-y-2">
-            <Image src="/product1.webp" width={300} height={300} alt="product 1" />
-            <h2 className="text-lg">Smart Phone</h2>
+            <Link href={"/product/1"}>
+                <Image src="/product1.webp" width={300} height={300} alt="product 1" />
+            </Link>
+            <Link href={"/product/1"}>
+                <h2 className="text-lg">Smart Phone</h2>
+            </Link>
             <p>$22.00</p>
             <div className="flex">
                 <AiFillStar />
@@ -13,7 +19,7 @@ const ProductItem = () => {
                 <AiFillStar />
                 <AiOutlineStar />
             </div>
-            <button className="btn btn-custom-yellow w-full hover:bg-black hover:text-custom-yellow">Add to cart</button>
+            <CustomButton paddingX={0} paddingY={5} customWidth="full" textSize="sm" buttonType="button" text="Add to cart"/>
         </div>
     )
 }
---
+++ utils/db.ts
@@ -0,0 +1,22 @@
+import {PrismaClient} from "@/prisma/generated/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+
+const adapter = new PrismaMariaDb(
+  process.env.DATABASE_URL!
+);
+
+const prismaClientSingleton = () => {
+    return new PrismaClient({adapter});
+}
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClientSingleton | undefined;
+}
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if(process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
